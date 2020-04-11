8ea81b957d965d7b1f79492c532242c8233fddae
Alexei <lesha18.02.2001@gmail.com>
2020-04-11 18:55:07 +0300

    Fix codestyle and add absolute path to docker-compose

--- lab2/blocks/footer.php
+++ lab2/blocks/footer.php
@@ -1,21 +1,13 @@
 <footer class="grid footer">
-  <div id="footer-site">
-    <div>
-      <ul class="widget-social">
-        <li>
-          <a href="" class="fa fa-vk"></a>
-        </li>
-        <li>
-          <a href="" class="fa fa-facebook"></a>
-        </li>
-        <li>
-          <a href="" class="fa fa-twitter"></a>
-        </li>
-        <li>
-          <a href="" class="fa fa-instagram"></a>
-        </li>
-      </ul>
-      <span>&copy; Каширский Алексей 2020</span>
+    <div id="footer-site">
+        <div>
+            <ul class="widget-social">
+                <li><a href="" class="fa fa-vk"></a></li>
+                <li><a href="" class="fa fa-facebook"></a></li>
+                <li><a href="" class="fa fa-twitter"></a></li>
+                <li><a href="" class="fa fa-instagram"></a></li>
+            </ul>
+            <span>&copy; Каширский Алексей 2020</span>
+        </div>
     </div>
-  </div>
 </footer>
