5faf99955cdd8490ba0778fa4be631256cb4d0b3
L3SHA <lesha18.02.2001@gmail.com>
2020-05-03 14:45:44 +0300

    Fix tabs collapse

--- Lab_8/site/service.php
+++ Lab_8/site/service.php
@@ -2,19 +2,18 @@
 
 <!DOCTYPE HTML>
 <html>
-		<head>
-				<meta charset = "utf-8" />
-				<title>Главная</title>
-				<link rel="stylesheet" type="text/css" href="css/style.css" />
-		</head>
-
-		<body>
-				<ul id="nav">
-						<li><a href="index.php">Главная</a></li>
-						<li><a href="service.php">Услуги</a></li>
-						<li><a href="contacts.php">Контакты</a></li>
-						<li><a href="about.php">О нас</a></li>
-				</ul>
-				<h2>Услуги</h2>
-		</body>
+    <head>
+        <meta charset = "utf-8"/>
+	<title>Главная</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+    </head>
+    <body>
+        <ul id="nav">
+	    <li><a href="index.php">Главная</a></li>
+	    <li><a href="service.php">Услуги</a></li>
+	    <li><a href="contacts.php">Контакты</a></li>
+	    <li><a href="about.php">О нас</a></li>
+	</ul>
+	<h2>Услуги</h2>
+    </body>
 </html>
